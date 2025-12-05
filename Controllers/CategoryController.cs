8ddc2df39cef29d0bfc579b15fc2ad74c544aae9
yasanka <yasankaj@99x.io>
2025-12-05 11:25:29 +0530

    feat: fetch and display categories

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bulky.Data;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bulky.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDBContext _dbContext;
+        public CategoryController(ApplicationDBContext db)
+        {
+            _dbContext = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _dbContext.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
