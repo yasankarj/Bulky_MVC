8d632503fdb741852a71e4cd30efcf1eb266fd12
yasanka <yasankaj@99x.io>
2025-12-05 11:25:07 +0530

    chore: added sample data into categories

--- Migrations/20251205053223_seed-category-table.Designer.cs
+++ Migrations/20251205053223_seed-category-table.Designer.cs
@@ -3,6 +3,7 @@ using Bulky.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bulky.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20251205053223_seed-category-table")]
+    partial class seedcategorytable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
